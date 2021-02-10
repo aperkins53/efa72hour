91405301dd1012bdb1c9eb568a72934a672c4b6c
Alec Perkins <alec.1824@gmail.com>
2021-02-09 20:34:28 -0500

    added image

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import './App.css';
 import NASA from './components/NASA';
 
 function App() {
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-  //const date = new Date(Date.now()).toLocaleDateString();
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+  
 
-  //4 urls - Nasa, Zomato, Weather, Jobs
-  const nasaurl = `https://api.nasa.gov/planetary/earth/imagery?${longitude}&${latitude}&date=2021-07-02&api_key=R4umF8g5DMrB6R2FNqPDMEMBPyUN9H9ni2VMZeZA`;
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(setLatAndLong);
+    if (navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(successCallback);
+    }
   }, [])
-
-  function setLatAndLong(){
-    setLatitude(coords.latitude);
-    setLongitude(coords.longitude);
+  
+  function successCallback(position){
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
   }
-
+  
   return (
     <div>
-      <NASA url={nasaurl} />
+      <NASA latitude={latitude} longitude={longitude} />
     </div>
   );
 }
--- src/components/NASA.js
+++ src/components/NASA.js
@@ -1,14 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Image } from 'react-bootstrap';
 
 const NASA = (props) => {
-    if (props.url){
-        //fetch to get image, return in div
-        //blob research
-    }
+    const [image, setImage] = useState('');
+
+    
+    useEffect(() => {
+        if (props.longitude === undefined || props.latitude === undefined) {
+            return;
+        }
+
+        console.log(props.latitude, props.longitude);
+
+        const url = `https://api.nasa.gov/planetary/earth/assets?lon=${props.longitude}&lat=${props.latitude}&date=2020-02-07&dim=0.5&api_key=R4umF8g5DMrB6R2FNqPDMEMBPyUN9H9ni2VMZeZA`;
+
+        if (url){
+            fetch(url)
+                .then(res => res.json())
+                .then(json => setImage(json.url))
+        }
+    }, [props.longitude, props.latitude]);
+
+    let styles = {img: {maxHeight: '400px', maxWidth: '350px', border: '2px solid black', borderRadius: '20%' }}
 
     return(
         <div>
-            <img src='#'></img>
+            <Image id='image' src={image} style={styles.img} />
         </div>
     );
 };
