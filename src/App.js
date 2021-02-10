d4ff25749509fe73f7ef3ec04e78d83cbfc86ffb
apple <apple@apples-MacBook-Air.local>
2021-02-09 20:51:36 -0500

    location fixed

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import './App.css';
 import GitJobsApp from './components/GitJobsApp'
 
 const App = () => {
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+
+  useEffect(() => {
+    if (navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(successCallback);
+    }
+    }, []);
+  function successCallback(position){
+    setLatitude(position.coords.latitude)
+    setLongitude(position.coords.longitude)
+  }
+
   return (
     <div>
-      <GitJobsApp />
+      <GitJobsApp latitude={latitude} longitude={longitude}/>
     </div>
   );
 }
--- src/components/GitJobsApp.jsx
+++ src/components/GitJobsApp.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import GitJobsResults from './GitJobsResults';
 import {
     Container, Row, CardColumns, Col
 } from 'reactstrap';
-const GitJobsApp = () => {
+
+
+const GitJobsApp = (props) => {
     const [results, setResults] = useState([]);
     function fetchResults() {
+        console.log(props.latitude, props.longitude)
         const corsURL = "https://efa-cors-anywhere.herokuapp.com/"
-        // const gitJobURL = `https://jobs.github.com/positions.json?${latitude}&${longitude}`
-        const gitJobURL = `https://jobs.github.com/positions.json?https://jobs.github.com/positions.json?lat=37.3229978&long=-122.0321823?limit=3`
+        const gitJobURL = `https://jobs.github.com/positions.json?lat=${props.latitude}&long=${props.longitude}`
+        // const gitJobURL = `https://jobs.github.com/positions.json?https://jobs.github.com/positions.json?lat=37.3229978&long=-122.0321823?limit=3`
         fetch(corsURL + gitJobURL)
             .then((res) => res.json())
             .then((json) => {
@@ -17,14 +20,16 @@ const GitJobsApp = () => {
             }
             )
     }
-
-    // useEffect(() => {
-    //     fetchResults();
-    // }, []);
+    useEffect(() => {
+        if (!props.latitude || !props.longitude) {
+            return
+        }
+        fetchResults();
+    }, [props.latitude, props.longitude]);
 
     function displayJobs() {
         return results.length > 0
-            ? results.splice(0, 4).map((result) =>
+            ? results.map((result) =>
                 <GitJobsResults jobs={result} />) : null;
     }
     return (
@@ -33,7 +38,7 @@ const GitJobsApp = () => {
                 <div>
                     <br />
                     <Col className="mt-auto">
-                        <button type="button" class="btn btn-lg" style={{ fontWeight: 700, color: "white", background: "rgb(37,87,167)" }} onClick={fetchResults}>Find Jobs</button>
+                        <button type="button" className="btn btn-lg" style={{ fontWeight: 700, color: "white", background: "rgb(37,87,167)" }} onClick={fetchResults}>Find Jobs</button>
                     </Col>
                     <br />
                 </div>
